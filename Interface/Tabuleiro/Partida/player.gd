ef5e0e75ae3aca4eed4af90ec43b8d3180dc0e83
VoxshaDracon <arthur.peixoto0551@gmail.com>
2025-02-11 23:14:39 -0300

    Tentativa de arrumar umas coisas
    -imagens do fundo
    -timer
    -imagens das cartas

--- Interface/Tabuleiro/Partida/player.gd
+++ Interface/Tabuleiro/Partida/player.gd
@@ -3,22 +3,30 @@ class_name Player
 
 signal on_mana_spend(big_mana, small_mana)
 signal on_mana_reset()
+signal set_time()
+signal use_time()
 
 var nickname : String
 var big_mana : int
 var small_mana : int
 var hand : Hand
+var time_left : float
+var time_pass : int
+var timer_set : bool = false
 
 func _init(nickname: String, hand: Node) -> void:
 	self.nickname = nickname
 	self.hand = hand
 	self.big_mana = 1
 	self.small_mana = 2
+	self.time_pass = 7200
+	self.time_left = 0
 
 func try_use_mana(big_mana: int, small_mana: int):
 	if self.big_mana >= big_mana and self.small_mana >= small_mana:
 		self.big_mana -= big_mana
 		self.small_mana -= small_mana
+		on_mana_spend.emit(self.big_mana, self.small_mana)
 		on_mana_spend.emit(self.big_mana, self.small_mana)
 		return true
 	else: #aprimorar essa parte
@@ -36,6 +44,11 @@ func change_mana():
 	else:
 		return false
 		
+func set_timer():
+	set_time.emit()
+	if timer_set == false:
+		timer_set = true
+	
 func buy_card():
 	var new_card = self.gm.buy_deck.draw_card()
 	if new_card:
@@ -48,8 +61,7 @@ func buy_card():
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
