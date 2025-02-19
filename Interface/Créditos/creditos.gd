852099ebd85ea7e553a4f8465f9398ed9a305c36
João Vitor <joaovitor20022004@gmail.com>
2025-02-18 21:30:37 -0300

    UI dos botões improvisada
    Alterando fonte, cor e tamanho dos botões, além de adicionar arte do lider dos ophidianos

--- Interface/Créditos/creditos.gd
+++ Interface/Créditos/creditos.gd
@@ -6,3 +6,12 @@ class_name CreditosMenu
 func _input(event):
 	if event.is_action_pressed("Esc"): 
 		var _chance_scene: bool = get_tree().change_scene_to_file(cena_inicial)
+
+func _ready() -> void: 
+	for _button in get_tree().get_nodes_in_group("button_creditos"):
+		_button.pressed.connect(_on_button_pressed.bind(_button))
+		
+func _on_button_pressed(_button : Button) -> void:
+	match _button.name:
+		"sair_button": 
+			get_tree().change_scene_to_file("res://Interface/Menu/tela_menu.tscn")
